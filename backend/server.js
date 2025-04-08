55c0740abcb3c21a0141c3e813b64cef2a0ef609
Aditya Tomar <adi02032002@gmail.com>
2025-04-08 22:39:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,53 @@
+// import express from "express";
+// import { SmartAPI } from "smartapi-javascript";
+// import authRoutes from "./src/routes/auth.route.js";
+// import userRoutes from "./src/routes/user.route.js";
+// import brokerRoutes from "./src/routes/broker.route.js";
+// import userBrokerRoutes from "./src/routes/user-broker.route.js";
+// import { connectDB } from "./src/config/database.js";
+// import cors from "cors";
+// import { config } from "dotenv";
+// import https from "https";
+// import fs from "fs";
+
+// config({
+//   path: "./.env",
+// });
+
+// const app = express();
+
+// // // 🔐 HTTPS Certificate
+// // const sslOptions = {
+// //   key: fs.readFileSync("./cert/server.key"),
+// //   cert: fs.readFileSync("./cert/server.cert"),
+// // };
+
+// let smartAPI = new SmartAPI({
+//   api_key: "smartapi_key",
+// });
+
+// connectDB();
+
+// app.use(
+//   cors({
+//     origin: "*",
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json({ limit: "50mb" }));
+// app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+// app.use("/api/v1/user", userRoutes);
+// app.use("/api/v1/auth", authRoutes);
+// app.use("/api/v1/brokers", brokerRoutes);
+// app.use("/api/v1/user-brokers", userBrokerRoutes);
+
+// // 🔄 Start HTTPS Server
+// https.createServer(sslOptions, app).listen(443, () => {
+//   console.log("🔐 HTTPS server running on https://localhost");
+// });
+
+
 import express from "express";
 import { SmartAPI } from "smartapi-javascript";
 import authRoutes from "./src/routes/auth.route.js";
@@ -7,20 +57,11 @@ import userBrokerRoutes from "./src/routes/user-broker.route.js";
 import { connectDB } from "./src/config/database.js";
 import cors from "cors";
 import { config } from "dotenv";
-import https from "https";
-import fs from "fs";
-
 config({
   path: "./.env",
 });
 
 const app = express();
-
-// 🔐 HTTPS Certificate
-const sslOptions = {
-  key: fs.readFileSync("./cert/server.key"),
-  cert: fs.readFileSync("./cert/server.cert"),
-};
 
 let smartAPI = new SmartAPI({
   api_key: "smartapi_key",
@@ -42,7 +83,8 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/brokers", brokerRoutes);
 app.use("/api/v1/user-brokers", userBrokerRoutes);
 
-// 🔄 Start HTTPS Server
-https.createServer(sslOptions, app).listen(443, () => {
-  console.log("🔐 HTTPS server running on https://localhost");
+app.listen(3000, () => {
+  console.log("App is live on 3000 port");
 });
+
+
